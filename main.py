8b104dfe17b809a51114a969463d7c3324489c79
SpiritCroc <dev@spiritcroc.de>
2022-01-16 14:24:03 +0100

    Cache contacts for faster responses

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 import yaml
 from flask import Flask, Response, request, abort
+import time
+from threading import Thread
 from waitress import serve
 
 from util import relative_path as rp
@@ -22,14 +24,31 @@ app = Flask("vcard-xml-bridge")
 
 @app.route(config["listen_path"], methods=['GET'])
 def request_get():
+    global contacts
     ip_addr = request.remote_addr
     if "ip_whitelist" in config and ip_addr not in config["ip_whitelist"]:
         print(f"Contacts requested by {ip_addr}, but not whitelisted - return 404!")
         abort(404)
-    print(f"Contacts requested by {ip_addr}, requesting from carddav...")
-    contacts = get_contacts()
-    print(f"Return xml contacts to {ip_addr}")
+    print(f"Contacts requested by {ip_addr}, handling")
     return Response(contacts, mimetype="text/xml")
+
+sleep_time = config["dav_update_interval"]
+
+def update_contacts_loop():
+    global contacts
+    while True:
+        time.sleep(sleep_time)
+        try:
+            print("Updating contacts...")
+            contacts = get_contacts()
+            print("Contacts updated")
+        except Exception as e:
+            print(f"Exception while trying to update contacts: {e}")
+
+print("Getting initial contacts...")
+contacts = get_contacts()
+print("Starting contact updater...")
+Thread(target=update_contacts_loop).start()
 
 host = config["listen_host"]
 port = config["listen_port"]
