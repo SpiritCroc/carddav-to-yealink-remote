2ac75f5041a4c672781a339be7891a848e091d01
Florian Best <best@univention.de>
2022-03-20 10:24:18 +0100

    style: fix some flake8 styling issues

--- util.py
+++ util.py
@@ -2,7 +2,8 @@ import inspect
 import os
 
 # Directory containing this file
-this_dir = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+this_dir = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
+
 
 def relative_path(path):
     return "{0}/{1}".format(this_dir, path)
