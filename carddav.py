6d49b57d26cb04e7c88620dd78b7d11e2bebd6e8
Florian Best <best@univention.de>
2022-03-21 10:39:55 +0100

    refactor: remove unused code

--- carddav.py
+++ carddav.py
@@ -26,10 +26,6 @@
 #-------------------------------------------------------------------------------
 # Lukasz Janyst:
 #
-# lxml encoding issue:
-# * Remove '<?xml version="1.0" encoding="utf-8"?>' header from responses
-#   to prevent etree errors
-#
 # requests-0.8.2:
 # * Remove the verify ssl flag - caused exception
 # * Add own raise_for_status for more meaningful error messages
@@ -42,11 +38,8 @@ contains the class PyCardDAV and some associated functions and definitions
 
 from collections import namedtuple
 import requests
-import sys
 import urllib.parse as urlparse
 import logging
-import lxml.etree as ET
-import string
 
 
 def raise_for_status(resp):
@@ -54,26 +47,6 @@ def raise_for_status(resp):
         msg = 'Error code: ' + str(resp.status_code) + '\n'
         msg += str(resp.content)
         raise requests.exceptions.HTTPError(msg)
-
-
-def get_random_href():
-    """returns a random href"""
-    import random
-    tmp_list = list()
-    for _ in range(3):
-        rand_number = random.randint(0, 0x100000000)
-        tmp_list.append("{0:x}".format(rand_number))
-    return "-".join(tmp_list).upper()
-
-
-DAVICAL = 'davical'
-SABREDAV = 'sabredav'
-UNKNOWN = 'unknown server'
-
-
-class UploadFailed(Exception):
-    """uploading the card failed"""
-    pass
 
 
 class PyCardDAV(object):
@@ -117,50 +90,8 @@ class PyCardDAV(object):
         raise_for_status(response)  # raises error on not 2XX HTTP status code
 
     @property
-    def verify(self):
-        """gets verify from settings dict"""
-        return self._settings['verify']
-
-    @verify.setter
-    def verify(self, verify):
-        """set verify"""
-        self._settings['verify'] = verify
-
-    @property
     def headers(self):
         return dict(self._default_headers)
-
-    def _check_write_support(self):
-        """checks if user really wants his data destroyed"""
-        if not self.write_support:
-            sys.stderr.write("Sorry, no write support for you. Please check "
-                             "the documentation.\n")
-            sys.exit(1)
-
-    def _detect_server(self):
-        """detects CardDAV server type
-        currently supports davical and sabredav (same as owncloud)
-        :rtype: string "davical" or "sabredav"
-        """
-        response = requests.request('OPTIONS',
-                                    self.url.base,
-                                    headers=self.header)
-        if "X-Sabre-Version" in response.headers:
-            server = SABREDAV
-        elif "X-DAViCal-Version" in response.headers:
-            server = DAVICAL
-        else:
-            server = UNKNOWN
-        logging.info(server + " detected")
-        return server
-
-    def get_abook(self):
-        """does the propfind and processes what it returns
-        :rtype: list of hrefs to vcards
-        """
-        xml = self._get_xml_props()
-        abook = self._process_xml_props(xml)
-        return abook
 
     def get_vcard(self, href):
         """
@@ -173,122 +104,3 @@ class PyCardDAV(object):
                                     **self._settings)
         raise_for_status(response)
         return response.content
-
-    def update_vcard(self, card, href, etag):
-        """
-        pushes changed vcard to the server
-        card: vcard as unicode string
-        etag: str or None, if this is set to a string, card is only updated if
-              remote etag matches. If etag = None the update is forced anyway
-         """
-        # TODO what happens if etag does not match?
-        self._check_write_support()
-        remotepath = str(self.url.base + href)
-        headers = self.headers
-        headers['content-type'] = 'text/vcard'
-        if etag is not None:
-            headers['If-Match'] = etag
-        self.session.put(remotepath, data=card.encode('utf-8'), headers=headers,
-                         **self._settings)
-
-    def delete_vcard(self, href, etag):
-        """deletes vcard from server
-        deletes the resource at href if etag matches,
-        if etag=None delete anyway
-        :param href: href of card to be deleted
-        :type href: str()
-        :param etag: etag of that card, if None card is always deleted
-        :type href: str()
-        :returns: nothing
-        """
-        # TODO: what happens if etag does not match, url does not exist etc ?
-        self._check_write_support()
-        remotepath = str(self.url.base + href)
-        headers = self.headers
-        headers['content-type'] = 'text/vcard'
-        if etag is not None:
-            headers['If-Match'] = etag
-        result = self.session.delete(remotepath,
-                                     headers=headers,
-                                     **self._settings)
-        raise_for_status(result)
-
-    def upload_new_card(self, card):
-        """
-        upload new card to the server
-        :param card: vcard to be uploaded
-        :type card: unicode
-        :rtype: tuple of string (path of the vcard on the server) and etag of
-                new card (string or None)
-        """
-        self._check_write_support()
-        card = card.encode('utf-8')
-        for _ in range(0, 5):
-            rand_string = get_random_href()
-            remotepath = str(self.url.resource + rand_string + ".vcf")
-            headers = self.headers
-            headers['content-type'] = 'text/vcard'
-            headers['If-None-Match'] = '*'
-            response = requests.put(remotepath, data=card, headers=headers,
-                                    **self._settings)
-            if response.ok:
-                parsed_url = urlparse.urlparse(remotepath)
-
-                if 'etag' not in response.headers:
-                    etag = ''
-                else:
-                    etag = response.headers['etag']
-
-                return (parsed_url.path, etag)
-        raise_for_status(response)
-
-    def _get_xml_props(self):
-        """PROPFIND method
-        gets the xml file with all vcard hrefs
-        :rtype: str() (an xml file)
-        """
-        headers = self.headers
-        headers['Depth'] = '1'
-        response = self.session.request('PROPFIND',
-                                        self.url.resource,
-                                        headers=headers,
-                                        **self._settings)
-        raise_for_status(response)
-        if response.headers['DAV'].count('addressbook') == 0:
-            raise Exception("URL is not a CardDAV resource")
-
-        return response.content
-
-    @classmethod
-    def _process_xml_props(cls, xml):
-        """processes the xml from PROPFIND, listing all vcard hrefs
-        :param xml: the xml file
-        :type xml: str()
-        :rtype: dict() key: href, value: etag
-        """
-        xml = string.replace(xml, '<?xml version="1.0" encoding="utf-8"?>', '', 1)
-        namespace = "{DAV:}"
-
-        element = ET.XML(xml)
-        abook = dict()
-        for response in element.iterchildren():
-            if (response.tag == namespace + "response"):
-                href = ""
-                etag = ""
-                insert = False
-                for refprop in response.iterchildren():
-                    if (refprop.tag == namespace + "href"):
-                        href = refprop.text
-                    for prop in refprop.iterchildren():
-                        for props in prop.iterchildren():
-                            if (props.tag == namespace + "getcontenttype" and
-                                (props.text == "text/vcard" or
-                                 props.text == "text/vcard; charset=utf-8" or
-                                 props.text == "text/x-vcard" or
-                                 props.text == "text/x-vcard; charset=utf-8")):
-                                insert = True
-                            if (props.tag == namespace + "getetag"):
-                                etag = props.text
-                        if insert:
-                            abook[href] = etag
-        return abook
